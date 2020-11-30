bab50206bee44b12139fbbee77ec7023374ceee3
M.TahirNazir <mtahirnazir788@gmail.com>
2020-11-30 08:48:08 -0800

    child2 updated

--- src/Child2.js
+++ src/Child2.js
@@ -7,6 +7,7 @@ const Child2 = () => {
     return (
         <div>
             <h1>This is using Reducer</h1>
+            <h3>This is Child2</h3>
             <h3>Value of reducer state is {state}</h3>
             <button onClick={()=>dispatch('INCREMENT')}>Increment Reducer</button>
         </div>
